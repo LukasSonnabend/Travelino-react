d0c3d0bd61561a4e56dfe6075a10016d34c64360
LukasSonnabend <lukassonnabend@outlook.com>
2020-08-17 23:02:04 +0200

    City Tables now working from Mock Api Data (France)

--- src/index.jsx
+++ src/index.jsx
@@ -28,7 +28,7 @@ class MainSearch extends React.Component {
             clicked: "", 
             input: "",
             data: {},
-            mockData: {"country":"France","response":{"more":true,"results":[{"id":"Paris","name":"Paris","score":9.9823657869383,"snippet":"The \"City of Light\" and one of the most visited places on Earth: romance, cuisine, the Eiffel Tower and a surprising amount of green await you."},{"id":"Strasbourg","name":"Strasbourg","score":8.88961573815986,"snippet":"Known for museums. Recommended places to visit are Strasbourg Cathedral, Palais Rohan and Musée de l’Œuvre Notre-Dame."},{"id":"Nice","name":"Nice","score":8.76918290264111,"snippet":"Known for dinner. Recommended places to visit are MAMAC, Villa Ephrussi de Rothschild and Villa Kerylos."},{"id":"Versailles2C_Yvelines","name":"Versailles","score":8.43516135381981,"snippet":"Known for museums. Recommended places to visit are Palace of Versailles, Hall of Mirrors and Versailles Orangerie."},{"id":"Bordeaux","name":"Bordeaux","score":8.39535923805795,"snippet":"Known for cycling. Recommended places to visit are Bordeaux Cathedral, Miroir d'eau and BETASOM."},{"id":"Lyon","name":"Lyon","score":8.39238444112527,"snippet":"Known for shopping. Recommended places to visit are Esplanade de Fourvière, St Jean Cathedral and Musée des Beaux-Arts de Lyon."},{"id":"Avignon","name":"Avignon","score":8.38060307227538,"snippet":"Known for dinner. Recommended places to visit are Palais des Papes, Pont Saint-Bénézet and Musée du Petit Palais."},{"id":"Marseille","name":"Marseille","score":8.37768668946974,"snippet":"Known for museums. Recommended places to visit are Notre-Dame de la Garde, Château d'If and Marseille Cathedral."},{"id":"Amiens","name":"Amiens","score":8.32163175161971,"snippet":"Known for drinks. Recommended places to visit are Amiens Cathedral, Musée de Picardie and Amiens Synagogue."},{"id":"NC3AEmes","name":"Nîmes","score":8.2520291829197,"snippet":"Known for lunch. Recommended places to visit are Pont du Gard, Arènes de Nîmes and Maison Carrée."}]}}
+            mockData: {"country":"France","response":{"more":true,"results":[{"id":"Paris","name":"Paris","score":9.9823657869383,"snippet":"The City of Light and one of the most visited places on Earth: romance, cuisine, the Eiffel Tower and a surprising amount of green await you."},{"id":"Strasbourg","name":"Strasbourg","score":8.88961573815986,"snippet":"Known for museums. Recommended places to visit are Strasbourg Cathedral, Palais Rohan and Musée de l’Œuvre Notre-Dame."},{"id":"Nice","name":"Nice","score":8.76918290264111,"snippet":"Known for dinner. Recommended places to visit are MAMAC, Villa Ephrussi de Rothschild and Villa Kerylos."},{"id":"Versailles2C_Yvelines","name":"Versailles","score":8.43516135381981,"snippet":"Known for museums. Recommended places to visit are Palace of Versailles, Hall of Mirrors and Versailles Orangerie."},{"id":"Bordeaux","name":"Bordeaux","score":8.39535923805795,"snippet":"Known for cycling. Recommended places to visit are Bordeaux Cathedral, Miroir d'eau and BETASOM."},{"id":"Lyon","name":"Lyon","score":8.39238444112527,"snippet":"Known for shopping. Recommended places to visit are Esplanade de Fourvière, St Jean Cathedral and Musée des Beaux-Arts de Lyon."},{"id":"Avignon","name":"Avignon","score":8.38060307227538,"snippet":"Known for dinner. Recommended places to visit are Palais des Papes, Pont Saint-Bénézet and Musée du Petit Palais."},{"id":"Marseille","name":"Marseille","score":8.37768668946974,"snippet":"Known for museums. Recommended places to visit are Notre-Dame de la Garde, Château d'If and Marseille Cathedral."},{"id":"Amiens","name":"Amiens","score":8.32163175161971,"snippet":"Known for drinks. Recommended places to visit are Amiens Cathedral, Musée de Picardie and Amiens Synagogue."},{"id":"NC3AEmes","name":"Nîmes","score":8.2520291829197,"snippet":"Known for lunch. Recommended places to visit are Pont du Gard, Arènes de Nîmes and Maison Carrée."}]}}
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -115,9 +115,8 @@ class MainSearch extends React.Component {
 
     render(){
         return (
-            <div>
+            <div className="justify-content-center">
                 {console.log(this.layerProps)}
-                <p>This Shits Fucked</p>
                 <h5>Know where you are going?</h5>
                 {/* <input value={this.state.input} onChange={this.handleChange}/> */}
                 
@@ -129,14 +128,18 @@ class MainSearch extends React.Component {
                 <div className="row justify-content-center">
                     <button type="button" className="btn-light">Search</button>
                 </div>
-                <div className="col-md-8 well">
-                <VectorMap {...worldLowRes}  layerProps={this.layerProps} onChange={this.clicked}/>
+
+                <div className="row justify-content-center">
+                    <div className="col-11 col-md-8 col-xl-6">
+                <VectorMap {...worldLowRes}  layerProps={this.layerProps} onChange={this.clicked} currentLayers="{'fr'}"/>
+                    </div>
                 </div>
 
                 <p>Search destinations in: {this.determineDestination()}</p>
+                <img src="https://www.countryflags.io/fr/flat/64.png"></img>
+                <br></br>
                 <button className="btn-light" onClick={this.makeRequest}>Go</button>
-                <p>{JSON.stringify(this.state.data)}</p>
-                <p>Mock Data</p>
+                {/* <p>{JSON.stringify(this.state.data)}</p> */}
                 <ResultView resultData={this.state.data} country={this.state.clicked}/>
 
                 {/* <p>{this.showCities(this.state.mockData)}</p> */}
@@ -161,29 +164,53 @@ class ResultView extends React.Component {
         for (let result in x.results){
             let pushArr = [];
             pushArr.push(x["results"][result]["name"]);
-            pushArr.push(x["results"][result]["score"]);
+            //pushArr.push(x["results"][result]["score"]);
             // pushArr.push(x["response"]["results"][result]["snippet"]);
             topCities.push(pushArr);
-            console.log(topCities);
         }
     
-        this.setState({
-            topCities: topCities
-        })
+        // this.setState({
+        //     topCities: topCities
+        // })
+        console.log(topCities.length);
+        if (topCities.length > 1){
+        //try to export as table
+        let table = [];
+        let count = 0;
+        for (let i = 0; i < 5; i++){
+            let children = []
+            for (let j = 0; j < 2; j++){
+                let city = topCities[count][0]
+            children.push(<td> <a href={"../"+this.props.country.toLowerCase()+"/"+city.toLowerCase()}>{ count+1 + ". " +topCities[count]}</a></td>)
+                count++
+            }
+            table.push(<tr>{children}</tr>);
+        }
+        return table
+    }
 
-        return topCities; 
+
+        //return topCities; 
         }
  
     
 
     render() {
         return (
-                <div className="SearchResults well">
-                    <button onClick={() => this.cityResults(this.props.resultData)}>Get Cities</button>
+                <div className="SearchResults">
+                    {/* <button onClick={() => this.cityResults(this.props.resultData)}>Get Cities</button> */}
                     {this.props.country}
-                    <p>{this.topCities}</p>
-                    {this.state.topCities.map((city) => <p><a href={"../"+this.props.country.toLowerCase()+"/"+city[0].toLowerCase()}>{city[0]}</a> </p>)}
-                    
+                    <p>Top Cities</p>
+                    {/* {this.state.topCities.map((city) => <p><a href={"../"+this.props.country.toLowerCase()+"/"+city[0].toLowerCase()}>{city[0]}</a> </p>)} */}
+                    <div className="row justify-content-center">
+                        <div className="col-auto">
+                            <table className="table table-striped table-dark table-bordered table-responsive">
+                                <tbody>
+                                    {this.cityResults(this.props.resultData)}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                     {/* {this.cityResults(this.props.resultData)}
                     {topCities.map((city) => <p><a href={"../"+country.toLowerCase()+"/"+city[0].toLowerCase()}>{city[0]}</a> </p>)} */}
                 </div>
